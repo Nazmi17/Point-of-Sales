2a3a4ee3d535a52184f9dfcabe6a20e49d51d08a
Nazmi17 <ramadaninazmi47@gmail.com>
2025-08-06 21:02:19 +0700

    user management

--- src/validations/auth-validation.ts
+++ src/validations/auth-validation.ts
@@ -3,9 +3,34 @@ import z from "zod";
 export const loginSchemaForm = z.object({
   email: z
     .string()
-    .min(1, "Silahkan isi email anda")
-    .email("Tolong masukkan email yang valid"),
-  password: z.string().min(1, "Silahkan masukkan password anda"),
+    .min(1, "Alamat email dibutuhkan")
+    .email("Masukkan alamat email yang valid"),
+  password: z.string().min(1, "Kata sandi dibutuhkan"),
+});
+
+export const createUserSchema = z.object({
+  email: z
+    .string()
+    .min(1, "Alamat email dibutuhkan")
+    .email("Masukkan alamat email yang valid"),
+  password: z.string().min(1, "Kata sandi dibutuhkan"),
+  name: z.string().min(1, "Nama dibutuhkan"),
+  role: z.string().min(1, "Role dibutuhkan"),
+  avatar_url: z.union([
+    z.string().min(1, "URL gambar dibutuhkan"),
+    z.instanceof(File),
+  ]),
+});
+
+export const updateUserSchema = z.object({
+  name: z.string().min(1, "Nama dibutuhkan"),
+  role: z.string().min(1, "Peran dibutuhkan"),
+  avatar_url: z.union([
+    z.string().min(1, "URL gambar dibutuhkan"),
+    z.instanceof(File),
+  ]),
 });
 
 export type LoginForm = z.infer<typeof loginSchemaForm>;
+export type CreateUserForm = z.infer<typeof createUserSchema>;
+export type UpdateUserForm = z.infer<typeof updateUserSchema>;
